42370e63e42af0bf52724b67e9436b5d6601162d
Ben Jones <benwjones76@outlook.com>
2020-10-19 15:46:00 +0100

    Challenge 5: added top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,11 +1,23 @@
 require "oystercard"
 
 describe OysterCard do
-  it "expects balance to be available" do
+  it "#balance" do
     expect(subject).to respond_to(:balance)
   end
 
   it "expects balance to be 0" do
     expect(subject.balance).to eq 0
+  end
+
+  it "#top_up" do
+    expect(subject).to respond_to(:top_up).with(1).arguments
+  end
+
+  it "returns new value" do
+    expect(subject.top_up(10)).to eq 10
+  end
+
+  it "returns a changed balance" do
+    expect { subject.top_up(20) }.to change { subject.balance }.by(20)
   end
 end
