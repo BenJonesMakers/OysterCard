1b4d6ee0039912e1b9ce1163ec0ac912e9452c3e
Ben Jones <benwjones76@outlook.com>
2020-10-19 15:11:27 +0100

    Challenge 4: added balance then refactored

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
-describe OysterCard do
+require "oystercard"
 
+describe OysterCard do
+  it "expects balance to be available" do
+    expect(subject).to respond_to(:balance)
+  end
+
+  it "expects balance to be 0" do
+    expect(subject.balance).to eq 0
+  end
 end
