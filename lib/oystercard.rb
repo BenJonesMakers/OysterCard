fe8fac84cbc60e3396b30bbc57beb431e7460b98
Ben Jones <benwjones76@outlook.com>
2020-10-20 08:31:56 +0100

    Challenge 6 complete top up maximum

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,18 @@
 class OysterCard
 
 attr_reader(:balance)
+attr_reader(:MAX_AMOUNT)
 
   def initialize
     @balance = 0
+    @MAX_AMOUNT = 90
   end
 
   def top_up(amount)
-    @balance += amount
+    if (@balance + amount) > @MAX_AMOUNT
+      raise "Error: adding #{amount} will exceed the maximum value of #{@MAX_AMOUNT}"
+    else
+      @balance += amount
+    end
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,23 +1,32 @@
 require "oystercard"
 
 describe OysterCard do
-  it "#balance" do
-    expect(subject).to respond_to(:balance)
+  describe "#balance" do
+    it "responds" do
+      expect(subject).to respond_to(:balance)
+    end
+
+    it "expects starting balance to be 0" do
+      expect(subject.balance).to eq 0
+    end
   end
 
-  it "expects balance to be 0" do
-    expect(subject.balance).to eq 0
-  end
+  describe "#top_up" do
+    it "responds" do
+      expect(subject).to respond_to(:top_up).with(1).arguments
+    end
 
-  it "#top_up" do
-    expect(subject).to respond_to(:top_up).with(1).arguments
-  end
+    it "returns new value" do
+      expect(subject.top_up(10)).to eq 10
+    end
 
-  it "returns new value" do
-    expect(subject.top_up(10)).to eq 10
-  end
+    it "returns a changed balance" do
+      expect { subject.top_up(20) }.to change { subject.balance }.by(20)
+    end
 
-  it "returns a changed balance" do
-    expect { subject.top_up(20) }.to change { subject.balance }.by(20)
+    it "errors if balance will be greater than £90" do
+      amount = 91
+      expect {subject.top_up(91)}.to raise_error "Error: adding #{amount} will exceed the maximum value of #{subject.MAX_AMOUNT}"
+    end
   end
 end
